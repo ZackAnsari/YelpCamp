6448ccf11e36773ec38c88740512145bd517b407
Zack Ansari <zackansari1@gmail.com>
2023-10-17 22:33:04 -0400

    adding seeds

--- seeds/index.js
+++ seeds/index.js
@@ -21,7 +21,7 @@ const seedDB = async () => {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
-      author: '652cb425ed5f50495f934700',
+      author: '652f42f58e202e6b79feb115',
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
